f146e2cc2d5ad6425746fc05529389ae8cfab933
wiliammunchen <wiliam.munchen@gmail.com>
2026-03-23 23:19:21 -0300

    feat: implement transformStateWithClones with history tracking

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let stateCopy = { ...currentState };
+
+    switch (action.type) {
+      case 'addProperties':
+        stateCopy = {
+          ...stateCopy,
+          ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        stateCopy = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    currentState = stateCopy;
+    history.push(stateCopy);
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
